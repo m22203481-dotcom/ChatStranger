b22c781fe81558b38a6462e5a26bc4ef76fba6ea
Manish Singhal <m22203481@gmail.com>
2026-07-29 18:53:03 +0530

    Connect frontend to production socket server

--- client/services/socket.ts
+++ client/services/socket.ts
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5001", {
+export const socket = io(
+  "https://stranger-connect-4s2z.onrender.com",
+   {
   autoConnect: false,
 });
