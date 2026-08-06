32d6495d15116096a72c9feeb4b304f9cbb31f91
Manish Singhal <m22203481@gmail.com>
2026-08-07 00:01:08 +0530

    Migrate production domain to chatstranger.chat

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "https://connectstranger.vercel.app"
+      "https://chatstranger.chat"
     ],
     methods: ["GET", "POST"],
   },
